1c401826db06f7cf6ce665d12f61f21d187ab51c
MynorVásquez <mmynnorev@gmail.com>
2022-07-02 10:44:33 -0600

    Update Ioperacioens , Implementando Dependencias.

---
+++ Interfaces/Interfaces/Icliente.cs
@@ -0,0 +1,32 @@
+﻿using Modelos.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Interfaces.Interfaces
+{
+   public interface Icliente
+    {
+        interface ICrearCliente
+        {
+            void crear(Clientes cliente);
+        }
+        interface IEditarCliente
+        {
+            void editar(Clientes cliente);
+        }
+        interface IEliminarCliente
+        {
+            void eliminar(Clientes cliente);
+        }
+
+
+
+
+
+    }
+}
---
+++ Interfaces/Interfaces/Ioperaciones.cs
@@ -0,0 +1,28 @@
+﻿using Modelos.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Interfaces
+{
+   public interface Ioperacion
+    {
+        public interface IAhorro
+        {
+            double calAhorro(Clientes cliente);
+        }
+        interface ICalcISR
+        {
+            double calcularISR(Clientes cliente);
+        }
+
+        interface ICalcIVA
+        {
+            double calcIVA(Clientes cliente);
+        }
+
+
+    }
+}
