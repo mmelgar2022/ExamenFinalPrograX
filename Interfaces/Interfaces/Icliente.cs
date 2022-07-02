f5921ff8a388d94d660d6b3b7b53ff6dc20c4dba
MynorVásquez <mmynnorev@gmail.com>
2022-07-02 10:44:33 -0600

    Agregando Fucionabilidad Modelos, Interfaces

---
+++ Interfaces/Interfaces/Icliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Interfaces
+{
+   public interface Icliente
+    {
+        interface ICrearCliente
+        {
+            void crear(Clientes cliente);
+        }
+        interface IEditarCliente
+        {
+            void editar(Clientes cliente);
+        }
+        interface IEliminarCliente
+        {
+            void eliminar(Clientes cliente);
+        }
+
+
+
+
+
+    }
+}
---
+++ Models/Modelos/Ubicacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos.Modelos
+{
+    public class ubicacion
+
+    {
+        public int id { get; set; }
+        public string descripcion { get; set; }
+       
+    }
+}
